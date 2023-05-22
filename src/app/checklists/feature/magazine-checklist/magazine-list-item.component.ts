3bd9fa3488ca974c9dba69668129a9afdef4ae45
Scott <csschapker94@gmail.com>
2023-05-22 18:57:29 -0500

    Adds some details about the magazines.

--- src/app/checklists/feature/magazine-checklist/magazine-list-item.component.ts
+++ src/app/checklists/feature/magazine-checklist/magazine-list-item.component.ts
@@ -6,20 +6,35 @@ import { MagazineObject } from "../../data-access/magazine";
   template: `
     <ng-container *ngIf="magazine">
       <div
-        class="bg-stone-800 rounded-md text-gray-200 p-4 flex flex-col justify-between gap-4"
+        class="bg-stone-800 rounded-md text-gray-100 p-4 flex flex-col justify-between gap-4"
       >
-        <div (click)="expanded = !expanded">
+        <div class="relative" (click)="expanded = !expanded">
           <h5
             class="font-bold text-lg w-full overflow-hidden text-ellipsis whitespace-nowrap"
             [ngClass]="{ 'text-xl': !expanded }"
           >
             {{ magazine.title }}
           </h5>
+          <div *ngIf="expanded">
+            <span>Collected: 0</span>
+            <span class="mx-2">|</span>
+            <span>Remaining: {{ magazine.issues.length }}</span>
+          </div>
+          <div class="absolute px-2 py-1 top-0 right-0 text-sm">
+            0/{{ magazine.issues.length }}
+          </div>
         </div>
 
         <ng-container *ngIf="expanded">
           <ul class="pl-1 flex flex-col gap-4">
-            <li class="relative" *ngFor="let issue of magazine.issues">
+            <li
+              class="relative text-gray-400"
+              *ngFor="let issue of magazine.issues"
+            >
+              <span
+                class="bg-green-700 px-2 pt-1 pb-1/2 absolute top-0 right-1 text-xs rounded-sm text-gray-50"
+                >FOUND</span
+              >
               <span class="block">{{ issue.title }}</span>
               <span class="block text-sm">{{ issue.location.name }}</span>
             </li>
