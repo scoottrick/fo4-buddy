ac0c5947431cfce3f668b826d78df997c5ad1d39
Scott <csschapker94@gmail.com>
2023-05-22 15:08:27 -0500

    Adds basic navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,27 @@
-import { Component, OnInit, inject } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { BobbleheadService } from "./checklists/data-access/bobblehead.service";
-import { MagazineService } from "./checklists/data-access/magazine.service";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "fo-root",
   template: `
-    <nav class="font-bold">
-      <ul>
-        <li><a routerLink="/checklists">Checklists</a></li>
-        <li><a routerLink="/hacking">Hacking</a></li>
-      </ul>
-    </nav>
-    <router-outlet></router-outlet>
+    <main class="fixed top-0 bottom-0 left-0 right-0 flex flex-col">
+      <section class="flex-grow overflow-y-scroll">
+        <router-outlet></router-outlet>
+      </section>
+
+      <section class="flex-none h-16 bg-slate-700 text-white">
+        <nav>
+          <ul class="flex flex-row justify-between gap-4">
+            <li class="flex-1 text-center">
+              <a routerLink="/checklists">Checklists</a>
+            </li>
+            <li class="flex-1 text-center">
+              <a routerLink="/hacking">Hacking</a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </main>
   `,
   styles: [],
 })
-export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  bobbleheadService = inject(BobbleheadService);
-  magazineService = inject(MagazineService);
-
-  bobbleheads$ = this.bobbleheadService.bobbleheads$;
-  magazines$ = this.magazineService.magazines$;
-
-  ngOnInit(): void {
-    this.bobbleheadService.fetchData();
-    this.magazineService.fetchData();
-  }
-}
+export class AppComponent {}
