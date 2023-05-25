82d4174f738ab847d1f969d70fcc1431b3b69ea8
Scott <csschapker94@gmail.com>
2023-05-25 11:50:04 -0500

    Adds a calculateLikeness function to compare two strings

--- src/app/hacking/data-access/terminal-hacking.service.ts
+++ src/app/hacking/data-access/terminal-hacking.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
-export class TerminalHackingService {}
+export class TerminalHackingService {
+  private terminalPasswords: string[] = [];
+  private terminalPasswordsSubject = new BehaviorSubject(
+    this.terminalPasswords
+  );
+
+  terminalPasswords$ = this.terminalPasswordsSubject.asObservable();
+
+  calculateLikeness(str1: string, str2: string): number {
+    let likeness = 0;
+    for (let i = 0; i < str1.length; i++) {
+      const char1 = str1.charAt(i).toLowerCase();
+      const char2 = str2.charAt(i).toLowerCase();
+      if (char1 === char2) {
+        likeness++;
+      }
+    }
+    return likeness;
+  }
+}
