e58223ce5d87f7006bbacc29624c22e044fa04fd
Scott <csschapker94@gmail.com>
2023-05-25 12:36:34 -0500

    Adds password input field

--- src/app/hacking/feature/terminal-hacking/terminal-hacking.component.ts
+++ src/app/hacking/feature/terminal-hacking/terminal-hacking.component.ts
@@ -24,13 +24,26 @@ const samplePasswords = [
   template: `
     <ng-container>
       <h1>terminal-hacking works!</h1>
-      <div></div>
+      <div class="p-4">
+        <mat-form-field class="w-full" color="accent">
+          <mat-label>Terminal passwords</mat-label>
+          <input matInput type="text" placeholder="" />
+          <mat-hint
+            >Enter the passwords from the in-game terminal separated by
+            spaces.</mat-hint
+          >
+        </mat-form-field>
+      </div>
       <div class="p-4 max-w-xl mx-auto">
         <mat-chip-listbox>
           <ng-container *ngFor="let word of passwords$ | async">
-            <mat-chip>
-              <span class="font-mono">{{ word }}</span>
-            </mat-chip>
+            <mat-chip-option
+              color="accent"
+              [selected]="word === 'takes'"
+              class="font-mono"
+            >
+              {{ word }}
+            </mat-chip-option>
           </ng-container>
         </mat-chip-listbox>
       </div>
@@ -41,7 +54,6 @@ const samplePasswords = [
       ::ng-deep .mdc-evolution-chip-set__chips {
         justify-content: space-evenly;
         row-gap: 1em;
-        align-items: center;
       }
     `,
   ],
--- src/app/hacking/hacking.module.ts
+++ src/app/hacking/hacking.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatChipsModule } from "@angular/material/chips";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 import { TerminalHackingComponent } from "./feature/terminal-hacking/terminal-hacking.component";
 
 @NgModule({
   declarations: [TerminalHackingComponent],
-  imports: [CommonModule, MatChipsModule],
+  imports: [CommonModule, MatChipsModule, MatFormFieldModule, MatInputModule],
 })
 export class HackingModule {}
