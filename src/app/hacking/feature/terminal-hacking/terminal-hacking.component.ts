b7744b60d6e244fc505562685c00df388719e960
Scott <csschapker94@gmail.com>
2023-05-25 12:19:52 -0500

    Adds simple chip layout for passwords.

--- src/app/hacking/feature/terminal-hacking/terminal-hacking.component.ts
+++ src/app/hacking/feature/terminal-hacking/terminal-hacking.component.ts
@@ -1,8 +1,51 @@
 import { Component } from "@angular/core";
+import { of } from "rxjs";
+
+const samplePasswords = [
+  "takes",
+  "known",
+  "kicks",
+  "stark",
+  "boots",
+  "baton",
+  "clear",
+  "crime",
+  "waste",
+  "close",
+  "sword",
+  "slave",
+  "fargo",
+  "maybe",
+  // "males",
+];
 
 @Component({
   selector: "fo-terminal-hacking",
-  template: ` <h1>terminal-hacking works!</h1> `,
-  styles: [],
+  template: `
+    <ng-container>
+      <h1>terminal-hacking works!</h1>
+      <div></div>
+      <div class="p-4 max-w-xl mx-auto">
+        <mat-chip-listbox>
+          <ng-container *ngFor="let word of passwords$ | async">
+            <mat-chip>
+              <span class="font-mono">{{ word }}</span>
+            </mat-chip>
+          </ng-container>
+        </mat-chip-listbox>
+      </div>
+    </ng-container>
+  `,
+  styles: [
+    `
+      ::ng-deep .mdc-evolution-chip-set__chips {
+        justify-content: space-evenly;
+        row-gap: 1em;
+        align-items: center;
+      }
+    `,
+  ],
 })
-export class TerminalHackingComponent {}
+export class TerminalHackingComponent {
+  passwords$ = of(samplePasswords);
+}
--- src/app/hacking/hacking.module.ts
+++ src/app/hacking/hacking.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatChipsModule } from "@angular/material/chips";
 
 import { TerminalHackingComponent } from "./feature/terminal-hacking/terminal-hacking.component";
 
 @NgModule({
   declarations: [TerminalHackingComponent],
-  imports: [],
+  imports: [CommonModule, MatChipsModule],
 })
 export class HackingModule {}
