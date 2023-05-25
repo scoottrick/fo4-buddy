7033e1ad711dc97ec7e5ebb75ce4716def3fc27c
Scott <csschapker94@gmail.com>
2023-05-25 18:35:31 -0500

    Adds basic layout for the hacking page

--- src/app/hacking/feature/terminal-hacking/terminal-hacking.component.ts
+++ src/app/hacking/feature/terminal-hacking/terminal-hacking.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { of } from "rxjs";
+import { Observable, combineLatest, map, of } from "rxjs";
 
 const samplePasswords = [
   "takes",
@@ -16,36 +16,90 @@ const samplePasswords = [
   "slave",
   "fargo",
   "maybe",
-  // "males",
+  "males",
 ];
 
 @Component({
   selector: "fo-terminal-hacking",
   template: `
-    <ng-container>
-      <h1>terminal-hacking works!</h1>
-      <div class="p-4">
-        <mat-form-field class="w-full" color="accent">
-          <mat-label>Terminal passwords</mat-label>
-          <input matInput type="text" placeholder="" />
-          <mat-hint
-            >Enter the passwords from the in-game terminal separated by
-            spaces.</mat-hint
-          >
-        </mat-form-field>
-      </div>
-      <div class="p-4 max-w-xl mx-auto">
-        <mat-chip-listbox>
-          <ng-container *ngFor="let word of passwords$ | async">
-            <mat-chip-option
-              color="accent"
-              [selected]="word === 'takes'"
-              class="font-mono"
-            >
-              {{ word }}
-            </mat-chip-option>
-          </ng-container>
-        </mat-chip-listbox>
+    <ng-container *ngIf="vm$ | async as vm">
+      <div class="max-w-2xl mx-auto p-4">
+        <!-- add words form -->
+        <ng-container>
+          <div class="py-4">
+            <form #addWordsForm (ngSubmit)="addWords($event)">
+              <mat-form-field class="w-full" color="accent">
+                <mat-label>Terminal passwords</mat-label>
+                <input matInput type="text" placeholder="" />
+                <mat-hint
+                  >Enter the passwords from the in-game terminal separated by
+                  spaces.</mat-hint
+                >
+                <button type="submit" matSuffix mat-icon-button color="accent">
+                  <mat-icon fontIcon="add"></mat-icon>
+                </button>
+              </mat-form-field>
+            </form>
+          </div>
+        </ng-container>
+
+        <!-- guess chips -->
+        <ng-container>
+          <div class="py-4">
+            <mat-chip-listbox>
+              <ng-container *ngFor="let word of vm.passwords">
+                <mat-chip-option color="accent" class="font-mono">
+                  {{ word }}
+                </mat-chip-option>
+              </ng-container>
+            </mat-chip-listbox>
+          </div>
+        </ng-container>
+
+        <!-- guess maker -->
+        <ng-container>
+          <div class="guess py-8 max-w-md mx-auto text-center">
+            <h1 class="text-center font-mono">{{ vm.activeGuess }}</h1>
+            <div class="text-center p-4">
+              <mat-form-field color="accent">
+                <mat-label>Likeness</mat-label>
+                <input
+                  matInput
+                  inputmode="numeric"
+                  min="0"
+                  [max]="vm.activeGuess.length"
+                  maxlength="2"
+                  value="0"
+                  class="text-center"
+                />
+              </mat-form-field>
+              <div class="text-center">
+                <button mat-flat-button color="accent">
+                  <mat-icon fontIcon="add"></mat-icon>
+                  Add Guess
+                </button>
+              </div>
+            </div>
+          </div>
+        </ng-container>
+
+        <!-- previous guesses -->
+        <ng-container>
+          <ul>
+            <ng-container *ngFor="let guess of vm.previousGuesses">
+              <li class="flex flex-row items-center py-1 font-mono">
+                <span class="grow">
+                  > {{ guess.word }}
+                  <br />
+                  > Likeness: {{ guess.likeness }}
+                </span>
+                <button mat-icon-button color="warn">
+                  <mat-icon fontIcon="delete"></mat-icon>
+                </button>
+              </li>
+            </ng-container>
+          </ul>
+        </ng-container>
       </div>
     </ng-container>
   `,
@@ -55,9 +109,44 @@ const samplePasswords = [
         justify-content: space-evenly;
         row-gap: 1em;
       }
+
+      .guess ::ng-deep .mat-mdc-form-field-infix {
+        width: auto;
+        min-width: 12em;
+        text-align: center;
+      }
+
+      .font-mono {
+        font-family: ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas,
+          "Liberation Mono", "Courier New", monospace;
+      }
+
+      .guess h1 {
+        font-size: 36px;
+      }
     `,
   ],
 })
 export class TerminalHackingComponent {
-  passwords$ = of(samplePasswords);
+  private passwords$ = of(samplePasswords);
+  private activeGuess$ = this.passwords$.pipe(map((passwords) => passwords[0]));
+  private previousGuesses$: Observable<{ word: string; likeness: number }[]> =
+    this.passwords$.pipe(
+      map((passwords) => {
+        return [
+          { word: passwords[0], likeness: 3 },
+          { word: passwords[1], likeness: 1 },
+        ];
+      })
+    );
+
+  vm$ = combineLatest({
+    passwords: this.passwords$,
+    activeGuess: this.activeGuess$,
+    previousGuesses: this.previousGuesses$,
+  });
+
+  addWords(event: Event) {
+    console.log("add");
+  }
 }
--- src/app/hacking/hacking.module.ts
+++ src/app/hacking/hacking.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
 import { MatChipsModule } from "@angular/material/chips";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
 
 import { TerminalHackingComponent } from "./feature/terminal-hacking/terminal-hacking.component";
 
 @NgModule({
   declarations: [TerminalHackingComponent],
-  imports: [CommonModule, MatChipsModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+  ],
 })
 export class HackingModule {}
