bfe08ebf339996b54cd8d3c2c3babd0de590b67a
Scott <csschapker94@gmail.com>
2023-06-05 11:31:53 -0500

    Uses ellipses to keep magazine issue titles on one line

--- src/app/checklists/feature/magazine-checklist/issue-list-item.component.ts
+++ src/app/checklists/feature/magazine-checklist/issue-list-item.component.ts
@@ -4,6 +4,19 @@ import { MagazineIssueObject } from "../../data-access/magazine";
 @Component({
   selector: "fo-issue-list-item",
   templateUrl: "./issue-list-item.component.html",
+  styles: [
+    `
+      ::ng-deep .mat-mdc-checkbox,
+      ::ng-deep .mdc-form-field {
+        width: 100%;
+      }
+
+      ::ng-deep .mdc-label {
+        flex-grow: 1;
+        overflow: hidden;
+      }
+    `,
+  ],
 })
 export class IssueListItemComponent {
   @Input() issue?: MagazineIssueObject;
