d0e84dcb76ffd458190f12bb0e7abf9933f76de7
Scott <csschapker94@gmail.com>
2023-12-20 18:58:33 -0600

    Removes old checklists route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { ChecklistShellComponent } from "./checklists/feature/checklist-shell/checklist-shell.component";
 import { TerminalHackingComponent } from "./hacking/feature/terminal-hacking/terminal-hacking.component";
 import { CollectionsShellComponent } from "./collections/feature/collections-shell/collections-shell.component";
 import { BobbleheadChecklistComponent } from "./checklists/feature/bobblehead-checklist/bobblehead-checklist.component";
 import { MagazineChecklistComponent } from "./checklists/feature/magazine-checklist/magazine-checklist.component";
 
 const routes: Routes = [
-  {
-    path: "checklists",
-    title: "Collections",
-    component: ChecklistShellComponent,
-  },
   {
     path: "hacking",
     title: "Terminal Hacking",
