fc0702da1dfa57004272d6529a8be299ee7e3afa
Scott <csschapker94@gmail.com>
2023-12-22 16:07:03 -0600

    Adds companion information in a basic layout.

--- src/app/checklists/feature/companion-list/companion-list.component.ts
+++ src/app/checklists/feature/companion-list/companion-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, computed, inject } from "@angular/core";
-import { combineLatest } from "rxjs";
 import { CompanionService } from "../../data-access/companion.service";
 import { CommonModule } from "@angular/common";
 
@@ -9,14 +8,53 @@ import { CommonModule } from "@angular/common";
   imports: [CommonModule],
   template: `
     <ul>
-      <li *ngFor="let companion of vm().companions">{{ companion.name }}</li>
+      <li class="py-4 px-8" *ngFor="let companion of companions()">
+        <div class="text-xl">{{ companion.name }}</div>
+        <div *ngIf="companion.perk.name !== '-'">
+          <div>Perk: {{ companion.perk.name }}</div>
+          <div>{{ companion.perk.url }}</div>
+        </div>
+        <div class="text-xs mt-1">
+          <div class="text-gray-200">Possible Locations</div>
+          <ul>
+            <li *ngFor="let location of companion.locations">
+              - {{ location.name }}
+            </li>
+          </ul>
+        </div>
+        <div class="text-xs mt-1">
+          <div class="text-gray-200">Prerequistes</div>
+          <div>{{ companion.prereq }}</div>
+        </div>
+        <div class="text-xs mt-1">
+          <div class="text-gray-200">SPECIAL</div>
+          <ul>
+            <li *ngFor="let stat of companion.specials">
+              <span class="p-2">S: {{ stat.strength }}</span
+              ><span class="p-2">P: {{ stat.perception }}</span
+              ><span class="p-2">E: {{ stat.endurance }}</span
+              ><span class="p-2">C: {{ stat.charisma }}</span
+              ><span class="p-2">I: {{ stat.intelligence }}</span
+              ><span class="p-2">A: {{ stat.agility }}</span
+              ><span class="p-2">L: {{ stat.luck }}</span>
+            </li>
+          </ul>
+        </div>
+        <div *ngIf="companion.quest" class="text-xs mt-1">
+          <div class="text-gray-200">Quest</div>
+          <div>{{ companion.quest.name }}</div>
+        </div>
+      </li>
     </ul>
   `,
   styles: ``,
 })
 export class CompanionListComponent {
   private companionService = inject(CompanionService);
-  vm = computed(() => ({
-    companions: this.companionService.companions(),
-  }));
+
+  companions = computed(() =>
+    this.companionService
+      .companions()
+      .sort((a, b) => a.name.localeCompare(b.name))
+  );
 }
