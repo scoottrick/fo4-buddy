502ef6686472d6c82eff2ce24e00cfe86f27263c
Scott <csschapker94@gmail.com>
2023-12-26 19:55:25 -0600

    Uses expansion panels for the companions list

--- src/app/checklists/feature/companion-list/companion-list.component.ts
+++ src/app/checklists/feature/companion-list/companion-list.component.ts
@@ -5,62 +5,72 @@ import { CompanionService } from "../../data-access/companion.service";
   selector: "fo-companion-list",
   template: `
     <ul>
-      <li class="py-4 px-8" *ngFor="let companion of companions()">
-        <div class="text-xl">{{ companion.name }}</div>
-        <div *ngIf="companion.perk">
-          <div>
-            Perk:
-            <fo-text-link
-              [href]="companion.perk.url | wikiUrl"
-              [text]="companion.perk.name"
-            />
-          </div>
-        </div>
-        <div class="text-xs mt-2">
-          <div class="text-gray-200">Possible Locations</div>
-          <ul>
-            <li *ngFor="let location of companion.locations">
+      <li class="py-2 px-8" *ngFor="let companion of companions()">
+        <mat-expansion-panel>
+          <mat-expansion-panel-header>
+            <mat-panel-title
+              ><ng-container
+                ><span class="text-xl">{{ companion.name }}</span></ng-container
+              ></mat-panel-title
+            >
+          </mat-expansion-panel-header>
+          <ng-container>
+            <div *ngIf="companion.perk">
+              <div>
+                Perk:
+                <fo-text-link
+                  [href]="companion.perk.url | wikiUrl"
+                  [text]="companion.perk.name"
+                />
+              </div>
+            </div>
+            <div class="text-xs mt-2">
+              <div class="text-gray-200">Possible Locations</div>
+              <ul>
+                <li *ngFor="let location of companion.locations">
+                  <fo-text-link
+                    text="{{ location.name }}"
+                    href="{{ location.url | wikiUrl }}"
+                  />
+                </li>
+              </ul>
+            </div>
+            <div class="text-xs mt-2">
+              <div class="text-gray-200">Requirements</div>
+              <div>{{ companion.requirements }}</div>
+            </div>
+            <div class="text-xs mt-2">
+              <div class="text-gray-200">SPECIAL</div>
+              <ul>
+                <li *ngFor="let stat of companion.specials">
+                  <span class="mr-2">S: {{ stat.strength }}</span
+                  ><span class="mr-2">P: {{ stat.perception }}</span
+                  ><span class="mr-2">E: {{ stat.endurance }}</span
+                  ><span class="mr-2">C: {{ stat.charisma }}</span
+                  ><span class="mr-2">I: {{ stat.intelligence }}</span
+                  ><span class="mr-2">A: {{ stat.agility }}</span
+                  ><span>L: {{ stat.luck }}</span>
+                </li>
+              </ul>
+            </div>
+            <div *ngIf="companion.quest" class="text-xs mt-1">
+              <div class="text-gray-200">Quest</div>
+              <div>{{ companion.quest.name }}</div>
+              <div>
+                <fo-text-link
+                  text="Quest Info"
+                  href="{{ companion.quest.url | wikiUrl }}"
+                />
+              </div>
+            </div>
+            <div *ngIf="companion.interestsUrl" class="text-sm mt-1">
               <fo-text-link
-                text="{{ location.name }}"
-                href="{{ location.url | wikiUrl }}"
+                text="Likes/Dislikes"
+                href="{{ companion.url + companion.interestsUrl | wikiUrl }}"
               />
-            </li>
-          </ul>
-        </div>
-        <div class="text-xs mt-2">
-          <div class="text-gray-200">Requirements</div>
-          <div>{{ companion.requirements }}</div>
-        </div>
-        <div class="text-xs mt-2">
-          <div class="text-gray-200">SPECIAL</div>
-          <ul>
-            <li *ngFor="let stat of companion.specials">
-              <span class="mr-2">S: {{ stat.strength }}</span
-              ><span class="mr-2">P: {{ stat.perception }}</span
-              ><span class="mr-2">E: {{ stat.endurance }}</span
-              ><span class="mr-2">C: {{ stat.charisma }}</span
-              ><span class="mr-2">I: {{ stat.intelligence }}</span
-              ><span class="mr-2">A: {{ stat.agility }}</span
-              ><span>L: {{ stat.luck }}</span>
-            </li>
-          </ul>
-        </div>
-        <div *ngIf="companion.quest" class="text-xs mt-1">
-          <div class="text-gray-200">Quest</div>
-          <div>{{ companion.quest.name }}</div>
-          <div>
-            <fo-text-link
-              text="Quest Info"
-              href="{{ companion.quest.url | wikiUrl }}"
-            />
-          </div>
-        </div>
-        <div *ngIf="companion.interestsUrl" class="text-sm mt-1">
-          <fo-text-link
-            text="Likes/Dislikes"
-            href="{{ companion.url + companion.interestsUrl | wikiUrl }}"
-          />
-        </div>
+            </div>
+          </ng-container>
+        </mat-expansion-panel>
       </li>
     </ul>
   `,
